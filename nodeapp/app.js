2faa61b5bb9149c326cdf8c6c56ec9e241eef6fa
qamarabbas572 <qamar5720@gmail.com>
2024-11-11 14:27:00 +0500

    commit first

---
+++ nodeapp/app.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const app = express()
+
+app.get("/", (req, res) =>{
+ res.send("Service is up and running");
+});
+
+app.listen(8080,() =>{
+    console.log("server is up")
+    
+});
---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+		terraform {
+			  required_providers {
+				aws = {
+				  source  = "hashicorp/aws"
+				  version = "~> 5.0"
+				}
+			  }
+              backend "s3" {
+                key = "aws/ec2-deploy/terraform.tfstate"
+              }
+			}
+provider "aws" {
+					  region     = var.region
+					
+					}           
+resource "aws_instance" "server" {
+					  ami           = "ami-0b20f552f63953f0e"
+					  instance_type = "t2.micro"
+					  key_name = aws_key_pair.deployer.key_name
+					  vpc_security_group_ids = [aws_security_group.main-sg.id]
+					  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+                      connection {
+                        type = "ssh"
+                        host = self.public_ip
+                        user = "ubuntu"
+                        private_key = var.private_key
+                        timeout = "4m"
+                      }
+
+					  tags = {
+						Name = "DeployVM"
+					  }
+					}
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-AUTH"
+}					
+resource "aws_key_pair" "deployer" {
+	key_name = var.key_name
+	public_key = var.public_key
+}
+resource "aws_security_group" "main-sg"{
+
+
+		#Inbound rule for HTTP
+		egress =[{
+			from_port = 0
+			description = ""
+			protocol = "all"
+			cidr_blocks = ["0.0.0.0/0"]
+			ipv6_cidr_blocks = []
+			prefix_list_ids = []
+			security_groups = []
+			self = false
+			to_port = 0
+
+		}]
+		#Outbound rule 
+		ingress =[ {
+			from_port = 22
+			to_port = 22
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+			description = ""
+			ipv6_cidr_blocks = []
+			prefix_list_ids = []
+			security_groups = []
+			self = false
+		},
+		{
+			from_port = 80
+			to_port = 80
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+			description = ""
+			ipv6_cidr_blocks = []
+			prefix_list_ids = []
+			security_groups = []
+			self = false
+		}]
+}
+output "intance_pubic_ip" {
+  value = aws_instance.server.public_ip
+  sensitive = true
+}
