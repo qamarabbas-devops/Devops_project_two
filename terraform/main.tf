f4f555e0874732c8d43c96fac68357c09d4ba20c
qamarabbas572 <qamar5720@gmail.com>
2024-11-12 11:31:09 +0500

    memory added

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 					}           
 resource "aws_instance" "server" {
 					  ami           = "ami-0b20f552f63953f0e"
-					  instance_type = "t2.micro"
+					  instance_type = "t2.medium"
 					  key_name = aws_key_pair.deployer.key_name
 					  vpc_security_group_ids = [aws_security_group.main-sg.id]
 					  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
